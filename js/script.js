5f4acf25c7ac9083ca2e6c4a38e94daef948e62c
malikaisaeva <121940266+malikaisaeva@users.noreply.github.com>
2023-03-23 13:24:37 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+//1 
+let id = Math.random()
+console.log(id);
+
+//2
+//let rand = Math.random()
+//let rt = rand.replace(/0/, '')
+//let rt2 = rand.replace(/\./, '')
+
+//console.log(rand);
+
+
+//2
+let number = Math.random();
+number = number.toString().slice(2)
+console.log(number);
